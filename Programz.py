3ac86ab75068b94136ceb398c84532f01813d9a3
shreechandra <100112433+shreechandra@users.noreply.github.com>
2022-07-01 18:30:14 +0530

    Update Programz.py

--- Programz.py
+++ Programz.py
@@ -263,3 +263,12 @@
 # print("\n")
 
 
+# Write a Python program to convert height (in feet and inches) to centimeters.
+# print("Input your height: ")
+# h_ft = int(input("Feet: "))
+# h_inch = int(input("Inches: "))
+# h_inch += h_ft * 12
+# h_cm = round(h_inch * 2.54, 1)
+# print("Your height is : %d cm." % h_cm)
+
+

d4783294dfb60633ec86d52efe4b52e804e4a235
shreechandra <100112433+shreechandra@users.noreply.github.com>
2022-07-06 18:18:00 +0530

    Update Programz.py

--- Programz.py
+++ Programz.py
@@ -486,3 +486,13 @@ print(p)"""
 """import os.path
 print(os.path.expanduser('~'))"""
 
+# 149. Write a Python function that takes a positive integer and returns the sum of the cube of all the positive integers smaller than the specified number.
+"""def sum_of_cubes(n):
+     n -= 1
+ total = 0
+ while n > 0:
+   total += n * n * n
+   n -= 1
+ return total
+print("Sum of cubes: ",sum_of_cubes(3))"""
+
